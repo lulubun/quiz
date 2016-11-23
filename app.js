c197942881142388cf5b737d75c69c4c3a132264
lulubun <laurapetitt@gmail.com>
2016-11-22 21:41:13 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+(function() {
+  var questions = [{
+    question: "Question 1: What is the name of the town the Gilmore Girls live in?",
+    choices: ["Gotham", "Hartford", "Stars Hollow", "Chilton"],
+    correctAnswer: 2
+    rightAnswerNote: "You have watched the show before..."
+    wrongAnswerNote: "I'm guessing you haven't ever watched the show."
+  }, {
+    question: "Question 2: What industry does Richard Gilmore work for?",
+    choices: ["newspapers", "shipping", "rubber duck production", "insurance"],
+    correctAnswer: 3,
+    rightAnswerNote: "R.G. isn't in the new series *sniff*",
+    wrongAnswerNote: "Either you got him confused with a Huntzberger or you have no idea who that person is..."
+  }, {
+  	question: "Question 3: At the end of Gilmore Girls, who does Rory end up with?",
+  	choices: ["Obama", "Logan", "Jess", "Dean"],
+  	correctAnswer: 0,
+  	rightAnswerNote: "Congrats, you're a Gilmore Girls nerd for knowing this one!",
+  	wrongAnswerNote: "Actually, it really is Obama. She becomes a reporter on his campaign trail. Yes, I know I've watched this show too much."
+  },{
+  	question: "Question 4: Which two characters are played by the same actress?",
+  	choices: ["Sasha and Sookie", "Michel and Liz", "Anna and Sasha", "Lane and Liz"],
+  	correctAnswer: 2,
+  	rightAnswerNote: "Seriously, did they think we wouldn't notice?!",
+  	wrongAnswerNote: "Yeah, there's really no way any sane person should know this information."
+  },{
+  	question: 'Question 5: Complete this sentence "Oy with the _____ already!"',
+  	choices: ["running", "poodles", "coffee", "noise"],
+  	correctAnswer: 1,
+  	rightAnswerNote: "Welcome to my little nerdy world. Pass the pop tarts.",
+  	wrongAnswerNote: "So I'm guessing you're a big 'Walking Dead' fan, right?"
+  }
